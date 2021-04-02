3e65e405afc1ea87e9cfeffecdd58bdf841ae965
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-04-01 19:49:11 -0700

    Created a new component CreateClassForm

---
+++ src/Components/CreateTaskForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CreateTaskForm extends Component {
+    state = {
+        title: "",
+        details: ""
+    };
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CreateTaskForm;
